1072b01ba024b3c69b574d20894c7438510276b2
Sergio Azconan <sergio.j.azcona@gmail.com>
2022-06-08 16:25:14 -0400

    Adding files anew

---
+++ arrays.rb
@@ -0,0 +1,22 @@
+#arrays CFU
+home_bodies = ["meg","SA", "LJ", "Ari"]
+ages = [37, 38, 4, 2]
+months = [.6, .11, .9, .7]
+male = [false, true, true, true]
+
+#pop will return the last element, i.e. Ari
+home_bodies.pop
+
+#push will add 81 to the end of the array
+ages.push(81)
+
+#unshift will add .12 to the start of the month array
+month.unshift(.12)
+
+#shift will return the value sited, ie false
+male.shift(false)
+
+#index position practice
+puts home_bodies [2] #returning LJ as he is in the 2nd index position
+puts ages [3] #returning 2
+puts months [0] #returning .6 
